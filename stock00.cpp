51168bc6ee318ec9309a62890b4b8ef53372e5de
Tagir Sultanov <togi94@mail.ru>
2017-04-18 02:06:36 +0300

    patch1

---
+++ stock00.cpp
@@ -0,0 +1,68 @@
+// stock00,cpp - реализация методов класса stock
+// версия 00
+#include<iostream>
+#include"stock00.h"
+void Stock::acquire(const std::string & co, long n, double pr)
+{
+	company = co;
+	if (n < 0)
+	{
+		std::cout << "Number fo shares can't be negative;"
+					 << company << "shares set to 0.\n";
+		shares = 0;
+	}
+	else
+		shares = n;
+	share_val = pr;
+	set_tot();
+}
+
+void Stock::buy(long num, double price)
+{
+	if (num < 0)
+	{
+		std::cout << "Number of shares purshased can't be negative. "
+				    << "Transaction is aborted.\n";
+	}
+	else 
+	{
+		shares += num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::sell(long num, double price)
+{
+	using std::cout;
+	if (num < 0)
+	{
+		cout << "Number of shares sold can't be negative. "
+			  << "Transaction is aborted.\n";
+	}
+	else if (num > shares)
+	{
+		cout << "You can't sell more than you have! "
+			  << "Transaction is aborted.\n";
+	}
+	else
+	{
+		shares -= num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::update(double price)
+{
+	share_val = price;
+	set_tot();
+} 
+
+void Stock::show()
+{
+	std::cout << "Company: " << company
+				 << "Shares: $ " << shares << '\n'
+				 << "Share price: $" << share_val
+				 << "Total Worth: $" << total_val << '\n';
+}
